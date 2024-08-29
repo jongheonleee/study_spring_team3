e953c76311b5d1983b6e194518f5129d91f85738
jongheonleee <qwefghnm1212@gmail.com>
2024-08-29 12:26:07 +0900

    회원 파트 개발 환경 세팅

--- src/main/java/com/example/shop2/constant/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.shop2.constant;
-
-public enum Role {
-    ADMIN, USER
-}
--- src/main/java/com/example/shop2/error/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.example.shop2.error;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    String name();
-    HttpStatus getHttpStatus();
-    String getMessage();
-}
